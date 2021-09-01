2b75ae774b6acbb7bc0f35830aa06d4cd164c756
Masa <katsukatsu.masa.0522@gmail.com>
2021-09-02 03:02:01 +0900

    firstcommit

---
+++ assets/data/user.js
@@ -0,0 +1,78 @@
+export default [
+  {
+    id: '1',
+    name: 'Saya',
+    image:
+      'https://pakutaso.cdn.rabify.me/shared/img/thumb/SAYA160312500I9A3721.jpg.webp?d=1420',
+    age: '21',
+    bio: '大学生',
+  },
+  {
+    id: '2',
+    name: 'Erika',
+    image:
+      'https://www.pakutaso.com/shared/img/thumb/TSJ86_date20150208082618_TP_V4.jpg',
+    age: '18',
+    bio: '高校生',
+  },
+  {
+    id: '3',
+    name: 'Hikaru',
+    image: 'https://www.pakutaso.com/shared/img/thumb/takebe2182601_TP_V.jpg ',
+    age: '25',
+    bio: '看護師',
+  },
+  {
+    id: '4',
+    name: 'Nana',
+    image: 'https://www.pakutaso.com/shared/img/thumb/mamechi1110015_TP_V.jpg',
+    age: '23',
+    bio: '美容師',
+  },
+  {
+    id: '5',
+    name: 'Kumi',
+    image:
+      'https://www.pakutaso.com/shared/img/thumb/fukugyo206140004_TP_V.jpg',
+    age: '28',
+    bio: '秘書',
+  },
+  {
+    id: '6',
+    name: 'Mirei',
+    image:
+      'https://www.pakutaso.com/shared/img/thumb/neruneruPAR54165_TP_V.jpg',
+    age: '20',
+    bio: 'パティシエ',
+  },
+  {
+    id: '7',
+    name: 'Ivan',
+    image: 'https://www.pakutaso.com/shared/img/thumb/model10211027_TP_V.jpg',
+    age: '26',
+    bio: 'モデル',
+  },
+  {
+    id: '8',
+    name: 'Misato',
+    image: 'https://www.pakutaso.com/shared/img/thumb/TSURU1891A036_TP_V.jpg',
+    age: '22',
+    bio: 'ライター',
+  },
+  {
+    id: '9',
+    name: 'Fuuka',
+    image:
+      'https://pakutaso.cdn.rabify.me/shared/img/thumb/saya160104294609.jpg.webp?d=1420',
+    age: '20',
+    bio: 'フリーター',
+  },
+  {
+    id: '10',
+    name: 'Wakana',
+    image:
+      'https://www.pakutaso.com/shared/img/thumb/tokyo0720IMGL7095_TP_V.jpg',
+    age: '24',
+    bio: '人事',
+  },
+];
