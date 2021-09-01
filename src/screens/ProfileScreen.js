01e6ef7d2a3119634b5f623f5758c8d2d6638107
Masa <katsukatsu.masa.0522@gmail.com>
2021-09-01 13:22:47 +0900

    first commit

---
+++ src/screens/ProfileScreen.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+
+export default Profilescreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>hello</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
