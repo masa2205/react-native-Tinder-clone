7450798454afc22ee6401896dd01b1fc2b88655d
Masa <katsukatsu.masa.0522@gmail.com>
2021-09-02 15:18:55 +0900

    text props add

--- src/components/MainImage.js
+++ src/components/MainImage.js
@@ -2,14 +2,22 @@ import React from 'react';
 import {View, Image, StyleSheet, Text, ImageBackground} from 'react-native';
 import ImageBottomBar from './ImageBottomBar';
 
-export default MainImage = () => {
+export default MainImage = props => {
+  const {name, image, age, bio} = props.user;
   return (
     <View style={styles.Imagecontainer}>
       <ImageBackground
         style={styles.Image}
         source={{
-          uri: 'https://reactnative.dev/img/tiny_logo.png',
+          uri: image,
         }}>
+        <View style={styles.nameAgeContainer}>
+          <Text style={styles.name}>{name}</Text>
+          <Text style={styles.age}>{age}</Text>
+        </View>
+        <View>
+          <Text style={styles.bio}>{bio}</Text>
+        </View>
         <ImageBottomBar />
       </ImageBackground>
     </View>
@@ -27,5 +35,26 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     overflow: 'hidden',
     justifyContent: 'flex-end',
+  },
+  nameAgeContainer: {
+    flexDirection: 'row',
+  },
+  name: {
+    color: 'white',
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginHorizontal: 10,
+  },
+  age: {
+    color: 'white',
+    fontSize: 25,
+    paddingTop: 5,
+  },
+  bio: {
+    color: 'white',
+    fontSize: 20,
+    lineHeight: 25,
+    marginHorizontal: 10,
+    paddingBottom: 5,
   },
 });
