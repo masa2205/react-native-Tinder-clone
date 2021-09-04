db441f5d932c210f9b26317c74ea268ec718081d
Masa <katsukatsu.masa.0522@gmail.com>
2021-09-04 15:07:47 +0900

    redux add

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import AppNavigator from './src/navigation/AppNavigator';
+import {Provider} from 'react-redux';
+import store from './store';
 
 export default App = () => {
   return (
+    // <Provider store={store}>
     <NavigationContainer>
       <AppNavigator />
     </NavigationContainer>
+    // </Provider>
   );
 };
